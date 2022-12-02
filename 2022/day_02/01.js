6dd6c24063e461138135401db3f2f8dcee2885e6
Matej Dostal <matej.dostal.sk@gmail.com>
2022-12-02 07:56:59 +0100

    day 2

---
+++ 2022/day_02/01.js
@@ -0,0 +1,26 @@
+// https://adventofcode.com/2022/day/2
+
+const fs = require("fs");
+
+const input = fs.readFileSync("input.txt", "utf8");
+const inputLines = input.split("\r\n");
+
+let totalScore = 0;
+
+const scoreMap = {
+    "A X": 1 + 3,
+    "A Y": 2 + 6,
+    "A Z": 3 + 0,
+    "B X": 1 + 0,
+    "B Y": 2 + 3,
+    "B Z": 3 + 6,
+    "C X": 1 + 6,
+    "C Y": 2 + 0,
+    "C Z": 3 + 3,
+};
+
+for (let i = 0; i < inputLines.length; i++) {
+    totalScore += scoreMap[inputLines[i]];
+}
+
+console.log(totalScore);
---
+++ 2022/day_02/02.js
@@ -0,0 +1,26 @@
+// https://adventofcode.com/2022/day/2
+
+const fs = require("fs");
+
+const input = fs.readFileSync("input.txt", "utf8");
+const inputLines = input.split("\r\n");
+
+let totalScore = 0;
+
+const scoreMap = {
+    "A X": 3 + 0,
+    "A Y": 1 + 3,
+    "A Z": 2 + 6,
+    "B X": 1 + 0,
+    "B Y": 2 + 3,
+    "B Z": 3 + 6,
+    "C X": 2 + 0,
+    "C Y": 3 + 3,
+    "C Z": 1 + 6,
+};
+
+for (let i = 0; i < inputLines.length; i++) {
+    totalScore += scoreMap[inputLines[i]];
+}
+
+console.log(totalScore);
