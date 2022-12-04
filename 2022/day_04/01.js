f3cb38481d6b4caf16ba3c05d038d887eea4a130
Matej Dostal <matej.dostal.sk@gmail.com>
2022-12-04 09:55:07 +0100

    day 4

---
+++ 2022/day_04/01.js
@@ -0,0 +1,20 @@
+// https://adventofcode.com/2022/day/4
+
+const fs = require("fs");
+
+const input = fs.readFileSync("input.txt", "utf8");
+const inputLines = input.split("\r\n");
+
+let count = 0;
+
+for (let i = 0; i < inputLines.length; i++) {
+    let pairs = inputLines[i].split(",").map((p) => p.split("-").map((n) => Number(n)));
+    if (
+        (pairs[0][0] <= pairs[1][0] && pairs[0][1] >= pairs[1][1]) ||
+        (pairs[0][0] >= pairs[1][0] && pairs[0][1] <= pairs[1][1])
+    ) {
+        count++;
+    }
+}
+
+console.log(count);
