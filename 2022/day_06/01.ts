52c0af8b2c77a9efd72b53627b82595431a0aa5c
Matej Dostal <matej.dostal.sk@gmail.com>
2022-12-06 07:11:52 +0100

    day 06

---
+++ 2022/day_06/01.ts
@@ -0,0 +1,19 @@
+// https://adventofcode.com/2022/day/6
+
+import * as fs from "fs";
+import * as path from "path";
+
+const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8");
+
+const LENGTH_OF_MARKER = 4;
+let result = -1;
+
+for (let i = LENGTH_OF_MARKER; i < input.length; i++) {
+    let set = new Set(input.substring(i - LENGTH_OF_MARKER, i).split(""));
+    if (set.size === LENGTH_OF_MARKER) {
+        result = i;
+        break;
+    }
+}
+
+console.log(result);
